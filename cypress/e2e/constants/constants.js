49e06d9e7d7d298e8c10d31503b673bc9ccc72ed
Luigi Aguirre <antonnyac.93@gmail.com>
2023-01-15 00:58:31 -0500

    Add new constants

--- cypress/e2e/constants/constants.js
+++ cypress/e2e/constants/constants.js
@@ -4,3 +4,17 @@ export const PRODUCT_SORT_OPTIONS = {
   lowToHigh: "Price (low to high)",
   highToLow: "Price (high to low)",
 };
+
+export const SUCCESS_MESSAGE = {
+  header: "THANK YOU FOR YOUR ORDER",
+  description:
+    "Your order has been dispatched, and will arrive just as fast as the pony can get there!",
+};
+
+export const PAGE_TITLES = {
+  products: "Products",
+  myCart: "Your Cart",
+  stepOne: "Checkout: Your Information",
+  stepTwo: "Checkout: Overview",
+  complete: "Checkout: Complete!",
+};
