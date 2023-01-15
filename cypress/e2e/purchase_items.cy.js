2aefc80056254761bf9b32fd0e898c71c62e5797
Luigi Aguirre <antonnyac.93@gmail.com>
2023-01-14 22:18:41 -0500

    Test POM implementation in purchase_items file

--- cypress/e2e/purchase_items.cy.js
+++ cypress/e2e/purchase_items.cy.js
@@ -1,64 +1,48 @@
 /// <reference types="Cypress" />
 
+import loginPage from "../pages/loginPage";
+import productsPage from "../pages/productsPage";
+import headerPage from "../pages/shared/headerPage";
+import cartCheckoutCompletePage from "../pages/shopping-cart/cartCheckoutCompletePage";
+import cartCheckoutOverviewPage from "../pages/shopping-cart/cartCheckoutOverviewPage";
+import cartCheckoutPage from "../pages/shopping-cart/cartCheckoutPage";
+import cartPage from "../pages/shopping-cart/cartPage";
+import { PRODUCT_SORT_OPTIONS } from "./constants/constants";
+
 describe("The user makes purchases of items through the Saucedemo App", () => {
   it("should log in successfully", () => {
     cy.visit("/");
-    cy.get("[data-test='username']").should("be.visible").type("standard_user");
-    cy.get("[data-test='password']").should("be.visible").type("secret_sauce");
-    cy.get("[data-test='login-button']").should("be.enabled").click();
-    cy.get("span[class='title']").should("have.text", "Products");
+    loginPage.login("standard_user", "secret_sauce");
 
     // 1. List of items
-    cy.get("[data-test='product_sort_container']").select(
-      "Price (high to low)"
-    );
-    cy.get(".inventory_item #item_4_img_link img").should("be.visible");
-    cy.get("[data-test='add-to-cart-sauce-labs-backpack']")
-      // .should("be.enabled")
-      .click();
-    cy.get(".shopping_cart_link")
-      .within((qnt) => {
-        cy.wrap(qnt).should("have.text", 1);
-      })
-      .click();
+    headerPage.verifyTitleIsDisplayed("Products");
+    headerPage.sortProductsBy(PRODUCT_SORT_OPTIONS.highToLow);
+    productsPage.selectProducts();
+    // cy.get(".inventory_item #item_4_img_link img").should("be.visible");
+    headerPage.verifyCartBadgeItems(1);
+    headerPage.navigateToCartPage();
 
     // 2. User Cart
-    cy.get(".header_secondary_container .title").should(
-      "have.text",
-      "Your Cart"
-    );
-    cy.get(".cart_item").should("be.visible");
-    cy.get("[data-test='checkout']").should("be.enabled").click();
+    headerPage.verifyTitleIsDisplayed("Your Cart");
+    cartPage.verifyItemsAddedAreDisplayed(1);
+    cartPage.clickOnCheckout();
 
     // 3. User Checkout
-    cy.get(".header_secondary_container .title").should(
-      "have.text",
-      "Checkout: Your Information"
-    );
+    headerPage.verifyTitleIsDisplayed("Checkout: Your Information");
 
-    cy.get("[data-test='firstName']").type("Test");
-    cy.get("[data-test='lastName']").type("Ing");
-    cy.get("[data-test='postalCode']").type("12312");
-    cy.get("[data-test='continue']").should("be.enabled").click();
+    cartCheckoutPage.completeCheckoutInformation("Test", "Ing", 12345);
+    cartCheckoutPage.clickOnContinue();
 
     // 4. User Checkout Overview
-    cy.get(".header_secondary_container .title").should(
-      "have.text",
-      "Checkout: Overview"
-    );
-    cy.get("[data-test='finish']").should("be.enabled").click();
+    headerPage.verifyTitleIsDisplayed("Checkout: Overview");
+    cartCheckoutOverviewPage.clickOnFinish();
 
     // 5. User Checkout Complete
-    cy.get(".header_secondary_container .title").should(
-      "have.text",
-      "Checkout: Complete!"
-    );
-    cy.get(".checkout_complete_container .complete-header").should(
-      "have.text",
+    headerPage.verifyTitleIsDisplayed("Checkout: Complete!");
+    cartCheckoutCompletePage.verifyThankyouHeaderIsDisplayed(
       "THANK YOU FOR YOUR ORDER"
     );
-    cy.get(".checkout_complete_container .complete-text").should(
-      "have.text",
+    cartCheckoutCompletePage.verifyThankyouMessageIsDisplayed(
       "Your order has been dispatched, and will arrive just as fast as the pony can get there!"
     );
   });
