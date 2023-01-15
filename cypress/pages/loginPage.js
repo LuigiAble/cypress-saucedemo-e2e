9c5371ff4d409b64378c3a8217c0ca6efca404c1
Luigi Aguirre <antonnyac.93@gmail.com>
2023-01-14 22:14:21 -0500

    Add POM elements & methods for Login

--- cypress/pages/loginPage.js
+++ cypress/pages/loginPage.js
@@ -1,3 +1,16 @@
-class LoginPage {}
+class LoginPage {
+  elements = {
+    usernameInput: () => cy.get("[data-test='username']"),
+    passwordInput: () => cy.get("[data-test='password']"),
+    loginButton: () => cy.get("[data-test='login-button']"),
+    errorMessage: () => cy.get("[data-test='error']"),
+  };
+
+  login(username, password) {
+    this.elements.usernameInput().type(username);
+    this.elements.passwordInput().type(password);
+    this.elements.loginButton().should("be.enabled").click();
+  }
+}
 
 module.exports = new LoginPage();
